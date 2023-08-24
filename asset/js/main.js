a91ba5ac904f48f0c70a10719712609c85d96843
FabioVasi <vasifabio96@gmail.com>
2023-08-24 17:13:45 +0200

    restart

--- asset/js/main.js
+++ asset/js/main.js
@@ -10,53 +10,29 @@ va applicato uno sconto del 40% per gli over 65.
 
 */
 
+/*
+Strumenti.
+
+- console log
+- getElementById/querrySelector
+- const/let
+- if/else
+- .classList/.className
+
+*/
+
 // chiedere numero di chilometri da percorrere
-
-const Km = Number(prompt('Quanti Km devi percorrere al costo di 0.21 € ?'));
-
-// chiedere l'età dell'utente
-
-const userAge = Number(prompt('Quanti anni hai?'));
-
-console.log(Km, userAge);
-
+const Km = document.querySelector()
+// chiedere Nome e Cognome dell'utente
+const userName = document.querySelector()
+// chiedere l'età del passeggero
+const userAge = document.querySelector()
+// prezzo del biglietto intero 0.21 € al km
+let ticketPrice = Km * 0.21;
 // applicare il 20% di sconto per i minorenni
-
 // applicare il 40% di sconto per gli over 65
-
-// calcolare il prezzo del viaggio
-
-let prezzoDiViaggio = Km * 0.21;
-
 if(userAge < 18){
-    prezzoDiViaggio = prezzoDiViaggio - (prezzoDiViaggio * 0.2);
+    ticketPrice = ticketPrice - (ticketPrice * 0.2);
 } else if(userAge > 65){
-    prezzoDiViaggio = prezzoDiViaggio - (prezzoDiViaggio * 0.4);
+    ticketPrice = ticketPrice - (ticketPrice * 0.4);
 }
-
-// stampare il messaggio con il prezzo in console includendo anche due cifre decimali
-
-console.log(prezzoDiViaggio.toFixed(2));
-
-// stampare il messaggio con il prezzo in pagina includendo anche due cifre decimali
-
-const speseElement = document.getElementById('spese');
-console.log(speseElement);
-
-speseElement.innerHTML = 'Ciao, il costo sarà pari a : ' + prezzoDiViaggio.toFixed(2) + ' €'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
